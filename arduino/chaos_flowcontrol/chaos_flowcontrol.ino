0c3055a5ba1a3d10df8c7d43c1a7799e5f230377
Johan Bichel Lindegaard <public@johan.cc>
2012-10-14 20:29:19 +0200

    arduino code updated

--- arduino/chaos_flowcontrol/chaos_flowcontrol.ino
+++ arduino/chaos_flowcontrol/chaos_flowcontrol.ino
@@ -1,22 +1,44 @@
+#define CHANNEL_NUM 5 
+
 int incomingByte = 0;   // for incoming serial data
 
 int state = 2; //0 = auto 1 = pending 2 = controlled
 long autoDelay = 400;
 long lastContact = 0;
 
-int channels [3];
+int channels [CHANNEL_NUM];
 
-float waterPressure [];
+float waterPressure [CHANNEL_NUM];
+boolean waterValveOpen [CHANNEL_NUM];
+boolean airValveOpen [CHANNEL_NUM];
+float airPressure [CHANNEL_NUM];
+
+int waterValvePins [CHANNEL_NUM];
+int airValvePins [CHANNEL_NUM];
+int pumpPins [CHANNEL_NUM]; 
+int airRegulatorPins [CHANNEL_NUM];
 
 // syntax
 // 2 W 1.0 
 
-
 void setup() {
-        Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
-        
-        
-        
+    Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
+  
+    waterValvePins[0] = 1;
+    waterValvePins[1] = 2;
+    waterValvePins[2] = 3;
+    
+    airValvePins[0] = 1;
+    airValvePins[1] = 2;
+    airValvePins[2] = 3;
+    
+    pumpPins[0] = 1;
+    pumpPins[1] = 2;
+    pumpPins[2] = 3;
+    
+    airRegulatorPins[0] = 1;
+    airRegulatorPins[1] = 2;
+    airRegulatorPins[2] = 3; 
 }
 
 void loop() {
